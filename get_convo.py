020a5c310b8510980f5571131105b900aff07ff2
Brian Rodriguez <rdgbrian@gmail.com>
2023-11-10 18:02:01 -0500

    get_convo.py has completed functionality

--- get_convo.py
+++ get_convo.py
@@ -3,47 +3,61 @@ import pandas as pd
 from convokit import Corpus, download
 
 # Set up command line arguments
+""" COmmented out for now
 parser = argparse.ArgumentParser(description='Process conversation dataset.')
 parser.add_argument('--min_messages', type=int, default=0, help='Minimum number of messages in a conversation')
 parser.add_argument('--has_personal_attack', type=bool, default=False, help='Whether the conversation includes a personal attack')
+parser.add_argument('--download',type=bool,default=False)
 args = parser.parse_args()
+print(args)
+"""
+min_messages = 0
+has_personal_attack = False
+year = 2018
+load = False # True -> need to download the dataset False -> if not
 
-# Download and load the corpus
-corpus = Corpus(filename=download("conversations-gone-awry-corpus"))
+DATA_DIR = 'data-convs-awry' #put the current dir here
+if load: #args.download:
+    AWRY_ROOT_DIR = download('conversations-gone-awry-corpus',data_dir=DATA_DIR)
+    print(AWRY_ROOT_DIR)
+else: 
+    AWRY_ROOT_DIR = 'data-convs-awry\\conversations-gone-awry-corpus' # if you are
 
-# Create DataFrames
-conversations_df = corpus.get_conversations_dataframe()
-utterances_df = corpus.get_utterances_dataframe()
-speakers_df = corpus.get_speakers_dataframe()
+awry_corpus = Corpus(AWRY_ROOT_DIR)
 
-# Merge DataFrames
-merged_df = pd.merge(utterances_df, speakers_df, left_on='speaker', right_index=True)
-merged_df = pd.merge(merged_df, conversations_df, left_on='conversation_id', right_index=True)
+def filter_conversations(corpus, min_messages = 0, has_personal_attack = None,year = None):
+  convos = []
+  for convo in corpus.iter_conversations():
+      # then use iter_utterances at the Conversation level to count the number of comments in each post
+      # (the Conversation-level iter_utterances iterates over Utterances in that Conversation only)
+      n_comments = len([u for u in convo.iter_utterances()])
+      if has_personal_attack != None:
+        convo_has_attack_filter = convo.meta['conversation_has_personal_attack'] == has_personal_attack
+      else:
+        convo_has_attack_filter = True
 
-# Filter based on command line arguments
-if args.min_messages > 0:
-    convo_counts = merged_df['conversation_id'].value_counts()
-    valid_convos = convo_counts[convo_counts >= args.min_messages].index
-    merged_df = merged_df[merged_df['conversation_id'].isin(valid_convos)]
+      if year != None:
+        year_filter = convo.meta['year'] == year
+      else:
+        year_filter = True
+      if (n_comments >= min_messages) and (convo_has_attack_filter) and (year_filter):
+          convos.append(convo)
+  return convos
 
-if args.has_personal_attack:
-    merged_df = merged_df[merged_df['meta.conversation_has_personal_attack'] == True]
+# you can add more attributes such as total amount of personal attacks and toxicity
+def convo_to_df(convo):
+  df = convo.get_utterances_dataframe()
+  df['conversation_id'] = convo.id
+  #you can create more attributes here
+  return df
 
-# Process each conversation
-for convo_id in merged_df['conversation_id'].unique():
-    convo_df = merged_df[merged_df['conversation_id'] == convo_id]
-    
-    # Calculate total toxicity and count personal attacks
-    total_toxicity = convo_df['meta.toxicity'].sum()
-    personal_attacks_count = convo_df['meta.comment_has_personal_attack'].sum()
+def convos_to_dfs(convos):
+  dfs = []
+  for convo in convos:
+    dfs.append(convo_to_df(convo))
+  return pd.concat(dfs) # do you want to concatinate the dfs or have the seperate dfs be 
 
-    print(f"Conversation ID: {convo_id}")
-    print(f"Total Toxicity: {total_toxicity}")
-    print(f"Personal Attacks: {personal_attacks_count}")
-    for _, row in convo_df.iterrows():
-        speaker_name = row['speaker']  # Assuming 'speaker' column contains speaker names
-        print(f"{speaker_name}: {row['text']}")
+# We can edit this file as we go to use it as a file used to import into other files
 
-    print("\n")
 
-# Optionally, save to a file or further process as needed
+#filter_conversations(awry_corpus,min_messages=50,has_personal_attack=True)
