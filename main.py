bbc5d28f30203dbf3916239eb6781e0141efa97b
Brian Rodriguez <rdgbrian@gmail.com>
2023-12-10 17:18:03 -0500

    port is now a int

--- main.py
+++ main.py
@@ -165,6 +165,6 @@ async def timeseries(query: TimeSeriesQuery):
 
   
 if __name__ == "__main__":
-    print(os.environ.get("PORT"),type(os.environ.get("PORT")))
-    uvicorn.run("main:app", host="0.0.0.0", port= os.environ.get("PORT") , reload=True, log_level="debug")
+    #print(os.environ.get("PORT"),type(os.environ.get("PORT")))
+    uvicorn.run("main:app", host="0.0.0.0", port= int(os.environ.get("PORT")) , reload=True, log_level="debug")
 
